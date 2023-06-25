6463bc061d9ab9aab120f8e16d1e551599b427bf
nikhil-mandlik <114665808+nikhil-mandlik-dev@users.noreply.github.com>
2023-06-25 15:43:49 +0530

    creating dialstyles

--- app/src/main/java/com/nikhil/here/watchface/MainActivity.kt
+++ app/src/main/java/com/nikhil/here/watchface/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.unit.dp
+import com.nikhil.here.watchface.ui.BrownStyle
 import com.nikhil.here.watchface.ui.Clock
+import com.nikhil.here.watchface.ui.MarronStyle
+import com.nikhil.here.watchface.ui.StandardClockStyle
 import com.nikhil.here.watchface.ui.lightStyle
+import com.nikhil.here.watchface.ui.theme.Brown
+import com.nikhil.here.watchface.ui.theme.Marron
 import com.nikhil.here.watchface.ui.theme.WatchfaceTheme
 
 
@@ -29,13 +37,77 @@ class MainActivity : ComponentActivity() {
         setContent {
             WatchfaceTheme {
                 // A surface container using the 'background' color from the theme
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+
+                HorizontalPager(
+                    pageCount = 4,
+                    modifier = Modifier.fillMaxSize(),
                 ) {
-                    Clock()
+                    when (it) {
+                        0 -> {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(Color.Black),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Clock(
+                                    modifier = Modifier
+                                        .size(360.dp),
+                                    clockStyle = StandardClockStyle
+                                )
+                            }
+                        }
+
+                        1 -> {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(Color.White),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Clock(
+                                    modifier = Modifier
+                                        .size(360.dp),
+                                    clockStyle = lightStyle
+                                )
+                            }
+                        }
+
+                        2 -> {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(Marron),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Clock(
+                                    modifier = Modifier
+                                        .size(360.dp),
+                                    clockStyle = MarronStyle
+                                )
+                            }
+                        }
+
+                        3 -> {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(Brown),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Clock(
+                                    modifier = Modifier
+                                        .size(360.dp),
+                                    clockStyle = BrownStyle
+                                )
+                            }
+                        }
+                    }
+
                 }
             }
         }
